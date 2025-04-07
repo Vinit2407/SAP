f94c6146236ecd2dd85cba840d02bde3dcf23b59
Vinit Patel <vinitpatel24072003@gmail.com>
2025-04-07 09:01:22 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://vinitpatel24:XzOufKiiB4W4bCPb@cluster0.uj5mvvc.mongodb.net/food-del').then(()=>console.log("DB Connected"));
+    await mongoose.connect('mongodb+srv://username:idstring:projectname').then(()=>console.log("DB Connected"));
 }
 
 
